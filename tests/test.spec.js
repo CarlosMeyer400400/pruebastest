a2cc991f26a2c486f5f0ff360af0c441702b3e8c
Yahir Hernandez Hernandez <20211035@UTHH.EDU.MX>
2024-02-20 21:05:14 -0600

    Agrega pruebas TestCafe

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,9 +1,25 @@
 import { Selector } from 'testcafe';
-
+//prueba de buscar h1 con el texto de Welcome to your Angular App
 fixture `Example Page`
     .page `http://localhost:4200`;
 
 test('should display welcome message', async t => {
     await t
     .expect(Selector('h1').withText('Welcome to your Angular App!').exists).ok();
+});
+
+//prueba de buscar boton por clase login-button
+
+test('should have a login button', async t => {
+    await t
+        .expect(Selector('.login-button').exists).ok();
+});
+
+
+//prueba de buscar a y h2 con texto 
+
+test('should navigate to about page', async t => {
+    await t
+        .click(Selector('a').withText('About'))
+        .expect(Selector('h2').withText('About Us').exists).ok();
 });
